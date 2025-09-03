c52ee188cc1125e1187cf5c733ef29477dedecad
ZixHD <havos25@gmail.com>
2025-09-03 20:51:42 +0200

    header started

---
+++ lmag_studio/components/LandingPage/Header/header.js
@@ -0,0 +1,36 @@
+import styles from './header.css'
+import Image from 'next/image';
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+export default function Header(){
+
+
+    return (
+        <div className="header-background">
+            <div className="header-container">
+                <div className="image-container">
+                    <Image src="/logo.gif"
+                            alt="logo"
+                            width={70}
+                            height={80}
+                        />
+                </div>
+                <div className="routes-container">
+                   <div className="route">
+                        <p className={inter.className}>Devlog</p>
+                    </div>
+                    <div className="route">
+                        <p className={inter.className}>About</p>
+                    </div>
+                    <div className="route">
+                        <p className={inter.className}>Contact</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
